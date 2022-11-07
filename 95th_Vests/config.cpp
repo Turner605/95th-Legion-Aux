c368c7933506907da549b418c57b1067629679bd
Turner605 <32025166+Turner605@users.noreply.github.com>
2022-11-07 12:52:38 +0000

    Finishing up the vests

--- 95th_Vests/config.cpp
+++ 95th_Vests/config.cpp
@@ -26,21 +26,14 @@ class cfgWeapons
     }; 
 
 	class JLTS_CloneVestHolster;
-     
-	class 95th_Vest_Base: JLTS_CloneVestHolster
+	class 95th_Vest_Trooper: JLTS_CloneVestHolster
 	{
 		author="95th Aux";
 		displayName="Clone Trooper Vest";
-		picture="\MRC\JLTS\characters\CloneArmor2\data\ui\CloneVestHolster_ui_ca.paa";
-		model="\MRC\JLTS\characters\CloneArmor2\CloneVestHolster.p3d";
-		hiddenSelections[]=
-		{
-			"camo1"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"\MRC\JLTS\characters\CloneArmor\data\Clone_vest_officer_co.paa"
-		};
+		// hiddenSelectionsTextures[]=
+		// {
+		// 	""
+		// };
 		class ItemInfo: VestItem
 		{
 			uniformModel="\MRC\JLTS\characters\CloneArmor2\CloneVestHolster.p3d";
@@ -50,6 +43,195 @@ class cfgWeapons
 				"camo1"
 			};
 			mass=80;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName="HitChest";
+					armor=6;
+					PassThrough=0.30000001;
+				};
+				class Legs
+				{
+					hitpointName="HitLegs";
+					armor=6;
+					passThrough=0.30000001;
+				};
+				class Arms
+				{
+					hitpointName="HitArms";
+					armor=4;
+					passThrough=0.30000001;
+				};
+			};
+		};
+	};
+
+	class JLTS_CloneVestKama;
+	class 95th_Vest_Veteran: JLTS_CloneVestKama
+	{
+		author="95th Aux";
+		displayName="Clone Veteran Vest";
+		// hiddenSelectionsTextures[]=
+		// {
+		// 	""
+		// };
+		class ItemInfo: VestItem
+		{
+			uniformModel="\MRC\JLTS\characters\CloneArmor\CloneVestKama.p3d";
+			containerClass="Supply100";
+			hiddenSelections[]=
+			{
+				"camo1"
+			};
+			mass=80;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName="HitChest";
+					armor=6;
+					PassThrough=0.30000001;
+				};
+				class Legs
+				{
+					hitpointName="HitLegs";
+					armor=6;
+					passThrough=0.30000001;
+				};
+				class Arms
+				{
+					hitpointName="HitArms";
+					armor=4;
+					passThrough=0.30000001;
+				};
+			};
+		};
+	};
+
+	class JLTS_CloneVestReconOfficer;
+	class 95th_Vest_Sergeant: JLTS_CloneVestReconOfficer
+	{
+		author="95th Aux";
+		displayName="Clone Sergeant Vest";
+		// hiddenSelectionsTextures[]=
+		// {
+		// 	""
+		// };
+		class ItemInfo: VestItem
+		{
+			uniformModel="\MRC\JLTS\characters\CloneArmor\CloneVestReconOfficer.p3d";
+			containerClass="Supply100";
+			hiddenSelections[]=
+			{
+				"camo1"
+			};
+			mass=80;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName="HitChest";
+					armor=6;
+					PassThrough=0.30000001;
+				};
+				class Legs
+				{
+					hitpointName="HitLegs";
+					armor=6;
+					passThrough=0.30000001;
+				};
+				class Arms
+				{
+					hitpointName="HitArms";
+					armor=4;
+					passThrough=0.30000001;
+				};
+			};
+		};
+	};
+
+	class JLTS_CloneVestOfficer;
+	class 95th_Vest_Captain: JLTS_CloneVestOfficer
+	{
+		author="95th Aux";
+		displayName="Clone Captain Vest";
+		// hiddenSelectionsTextures[]=
+		// {
+		// 	""
+		// };
+		class ItemInfo: VestItem
+		{
+			uniformModel="\MRC\JLTS\characters\CloneArmor\CloneVestOfficer.p3d";
+			containerClass="Supply100";
+			hiddenSelections[]=
+			{
+				"camo1"
+			};
+			mass=80;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName="HitChest";
+					armor=6;
+					PassThrough=0.30000001;
+				};
+				class Legs
+				{
+					hitpointName="HitLegs";
+					armor=6;
+					passThrough=0.30000001;
+				};
+				class Arms
+				{
+					hitpointName="HitArms";
+					armor=4;
+					passThrough=0.30000001;
+				};
+			};
+		};
+	};
+
+	class JLTS_CloneVestCommander;
+	class 95th_Vest_Lieutenant: JLTS_CloneVestCommander
+	{
+		author="95th Aux";
+		displayName="Clone Lieutenant Vest";
+		// hiddenSelectionsTextures[]=
+		// {
+		// 	""
+		// };
+		class ItemInfo: VestItem
+		{
+			uniformModel="\MRC\JLTS\characters\CloneArmor\CloneVestCommander.p3d";
+			containerClass="Supply100";
+			hiddenSelections[]=
+			{
+				"camo1"
+			};
+			mass=80;
+			class HitpointsProtectionInfo
+			{
+				class Chest
+				{
+					HitpointName="HitChest";
+					armor=6;
+					PassThrough=0.30000001;
+				};
+				class Legs
+				{
+					hitpointName="HitLegs";
+					armor=6;
+					passThrough=0.30000001;
+				};
+				class Arms
+				{
+					hitpointName="HitArms";
+					armor=4;
+					passThrough=0.30000001;
+				};
+			};
 		};
 	};
 }; 
