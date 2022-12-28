c8f6c957a2365e0c032ebe2989ecc0533050c0e7
Turner605 <32025166+Turner605@users.noreply.github.com>
2022-12-28 16:00:31 +0000

    Groundwork for squad insignias

---
+++ 95th_Insignias/config.cpp
@@ -0,0 +1,48 @@
+class CfgPatches
+{
+	class 95th_Legion_Insignias
+	{
+        author="95th Aux Team";
+        name="95th Legion Insignias";
+		requiredAddons[]=
+		{
+			"JLTS_characters_CloneArmor"
+		};
+		units[] = {};
+	};
+};
+
+class cfgUnitInsignia
+{
+	class 95th_squad_1
+	{
+		displayName = "Squad 1 Insiginia";
+		author="95th Aux Team";
+		texture="\95th_Insignias\Data\Squad_1.paa";
+		textureVehicle="";
+	}
+
+	class 95th_squad_2
+	{
+		displayName = "Squad 2 Insiginia";
+		author="95th Aux Team";
+		texture="\95th_Insignias\Data\Squad_2.paa";
+		textureVehicle="";
+	}
+
+	class 95th_squad_3
+	{
+		displayName = "Squad 3 Insiginia";
+		author="95th Aux Team";
+		texture="\95th_Insignias\Data\Squad_3.paa";
+		textureVehicle="";
+	}
+
+	class 95th_squad_4
+	{
+		displayName = "Squad 41 Insiginia";
+		author="95th Aux Team";
+		texture="\95th_Insignias\Data\Squad_4.paa";
+		textureVehicle="";
+	}
+}
