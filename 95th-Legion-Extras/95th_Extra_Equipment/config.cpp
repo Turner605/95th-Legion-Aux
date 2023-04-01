f9952d3f78533b413ac5daebae42aacef11a4a87
Turner605 <32025166+Turner605@users.noreply.github.com>
2023-04-01 16:28:17 +0100

    Removing flashbang

--- 95th-Legion-Extras/95th_Extra_Equipment/config.cpp
+++ 95th-Legion-Extras/95th_Extra_Equipment/config.cpp
@@ -5,8 +5,7 @@ class CfgPatches
         author="95th Aux Team";
         name="95th Legion Equipment";
 		requiredAddons[]={
-			"WBK_PhoenixTreal_FlamethrowerMOD",
-			"SUP_flash"
+			"WBK_PhoenixTreal_FlamethrowerMOD"
 		};
 		units[] = {};
 	};
@@ -16,11 +15,6 @@ class CfgAmmo
 {
 	class SUPER_flash_ammo;
 	class 95th_Concussion_Ammo: SUPER_flash_ammo {
-		model="3as\3AS_Equipment\model\3AS_Thermaldet.p3d";  
-	};
-
-	class ATLAS3_st_grenade_inc;
-	class 95th_Pyro_Ammo: ATLAS3_st_grenade_inc {
 		model="3as\3AS_Equipment\model\3AS_Thermaldet.p3d";  
 	};
 };
@@ -36,17 +30,6 @@ class cfgMagazines {
 		picture="\95th_Extra_Equipment\Data\Grenades\PyroGrenade.paa";
 		ammo = "95th_Pyro_Ammo";
 	};
-
-	class SUPER_flash;
-	class 95th_Concussion_Grenade: SUPER_flash {
-		author="95th Aux Team";
-		model="3as\3AS_Equipment\model\3AS_Thermaldet.p3d";
-		displayName="Concussion Grenade";
-		displayNameShort="Concussion";
-		descriptionShort="Type: Defensive Stun Grenade<br />Rounds: 1<br />Used in: Hand";
-		picture="\95th_Extra_Equipment\Data\Grenades\Concussion.paa";
-		ammo = "95th_Concussion_Ammo";
-	};
 }
 
 class CfgWeapons
@@ -55,33 +38,11 @@ class CfgWeapons
 	class Throw: GrenadeLauncher {
 		class ThrowMuzzle;
 
-		muzzles[]+={"95th_Pyro_Muzzle", "95th_Concussion_Muzzle"};
+		muzzles[]+={"95th_Pyro_Muzzle"};
 
 		class 95th_Pyro_Muzzle: ThrowMuzzle {
 			magazines[]= {"95th_Pyro_Grenade"};
 		};
-
-		class 95th_Concussion_Muzzle: ThrowMuzzle {
-			magazines[] = {"95th_Concussion_Grenade"};
-		};
-	};
-};
-
-class CfgFunctions {
-	class ninefiveth {
-		class FlashBang {
-			class flashThrown {
-				file = "\95th_Extra_Equipment\Scripts\flashThrown.sqf";
-			};
-		};
-	};
-};
-
-class Extended_PostInit_EventHandlers
-{
-	class 95th_Extra_Equip_PostInit
-	{
-		init="call compile preprocessFileLineNumbers '\95th_Extra_Equipment\Bootstrap\XEH_postInit.sqf'";
 	};
 };
 
