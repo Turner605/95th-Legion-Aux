e5b30a93be1c72841cfa1b9e86e4ca695bd45e94
Outcaztz <shadowkinss@gmail.com>
2023-04-25 14:57:01 +0200

    Swapping NVG to Facewear
    Swapped:
    - Sergeant Visor
    - Lieutenant/Captain combined and renamed to Command Visor

--- 95th-Legion-Main/95th_P1_Facewear/config.cpp
+++ 95th-Legion-Main/95th_P1_Facewear/config.cpp
@@ -34,4 +34,50 @@ class CfgGlasses
 		identityTypes[]={};
 		mass=4;
 	};
+	class 95th_P1_Visor_Sergeant: None
+	{
+		author="95th Aux";
+		displayname="[95th] Phase 1 Sergeant Visor";
+		model="\3AS\3AS_Characters\Clones\Headgear\3as_P1_visor.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"3AS\3AS_Characters\Clones\Headgear\Attachments\data\visor_co.paa",
+			"",
+			""
+		};
+		picture="\A3\Characters_F\data\ui\icon_g_combat_CA.paa";
+		identityTypes[]={};
+		mass=4;
+		class ItemInfo
+		{
+			type=616;
+			uniformModel="\3AS\3AS_Characters\Clones\Headgear\3as_P1_visor.p3d";
+			modelOff="\3AS\3AS_Characters\Clones\Headgear\3as_P1_visor.p3d";
+			modelOptics="\MRC\JLTS\Core_mod\nvg_optic_dummy.p3d";
+			mass=20;
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"camo2"
+			};
+		};
+	};
+	class 95th_P1_Visor_Command: 95th_P1_Visor_Sergeant
+	{
+		author="95th Aux Team";
+		displayname="[95th] Phase 1 Command Visor";
+		hiddenSelectionsTextures[]=
+		{
+			"3AS\3AS_Characters\Clones\Headgear\Attachments\data\visor_co.paa",
+			"3AS\3AS_Characters\Clones\Headgear\Attachments\data\accesories_co.paa",
+			"3AS\3AS_Characters\Clones\Headgear\Attachments\data\accesories_co.paa"
+		};
+	};
 };
