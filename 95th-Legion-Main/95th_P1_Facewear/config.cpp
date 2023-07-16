30c30fbd40b469eea8688dd68f808fce99691449
Turner605 <32025166+Turner605@users.noreply.github.com>
2023-07-16 17:53:39 +0100

    Fixing facewear

--- 95th-Legion-Main/95th_P1_Facewear/config.cpp
+++ 95th-Legion-Main/95th_P1_Facewear/config.cpp
@@ -23,25 +23,31 @@ class CfgGlasses {
 
 	class 95th_P1_Facewear_RangeFinder: None {
 		scope=2;
+		identityTypes[]={};
 		author="95th Aux";
 		displayname="[95th] Phase 1 Trooper RangeFinder";
 		model="\3AS\3AS_Characters\Clones\Headgear\3as_P1_RangeFinder.p3d";
+		picture="\A3\Characters_F\data\ui\icon_g_combat_CA.paa";
 	};
 
 	class 95th_P1_Facewear_Lamps: None {
 		scope=2;
+		identityTypes[]={};
 		author="95th Aux";
 		displayname="[95th] Phase 1 Trooper Headlamps";
 		model="\3AS\3AS_Characters\Clones\Headgear\3as_P1_lamps.p3d";
+		picture="\A3\Characters_F\data\ui\icon_g_combat_CA.paa";
 	};
 
 	class 95th_P1_Visor_Sergeant: None {
 		scope=2;
+		identityTypes[]={};
 		author="95th Aux";
 		displayname="[95th] Phase 1 Sergeant Visor";
 		model="\3AS\3AS_Characters\Clones\Headgear\3as_P1_visor.p3d";
 		hiddenSelections[]={"camo", "camo1", "camo2"};
 		hiddenSelectionsTextures[]={"3AS\3AS_Characters\Clones\Headgear\Attachments\data\visor_co.paa", "", ""};
+		picture="\A3\Characters_F\data\ui\icon_g_combat_CA.paa";
 	};
 
 	class 95th_P1_Visor_Command: 95th_P1_Visor_Sergeant {
@@ -52,6 +58,7 @@ class CfgGlasses {
 			"3AS\3AS_Characters\Clones\Headgear\Attachments\data\accesories_co.paa",
 			"3AS\3AS_Characters\Clones\Headgear\Attachments\data\accesories_co.paa"
 		};
+		picture="\A3\Characters_F\data\ui\icon_g_combat_CA.paa";
 	};
 };
 
