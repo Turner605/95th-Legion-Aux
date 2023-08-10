a6f1f8a3e66742c909196f23575d30e239b3dd5f
Turner605 <32025166+Turner605@users.noreply.github.com>
2023-08-10 20:58:36 +0100

    Mild changes to vests

--- 95th-Legion-Main/95th_Vests/Macros/Basic Vests.hpp
+++ 95th-Legion-Main/95th_Vests/Macros/Basic Vests.hpp
@@ -1,16 +1,4 @@
-#define NEW_95TH_BASIC_VEST(name,baseclass) class 95th_Basic_Vest_##name## : ##baseclass## {\
-	author=95th Aux Team;\
-	displayName=[95th] Basic Vest;\
-	hiddenSelectionsTextures[]={\
-		\95th_Vests\Data\Basic\##name##_Vest_Bottom.paa,\
-		\95th_Vests\Data\Basic\##name##_Vest_Top.paa\
-	};\
-	class XtdGearInfo {\
-		model = 95th_Basic_Vests;\
-		Type = ##name##;\
-	};
-
-#define NEW_95TH_BASIC_VEST_ARMOUR_OVERRIDEN(name,baseclass) class 95th_Basic_Vest_##name## : ##baseclass## {\
+#define NEW_95TH_VEST_HOLSTER_TYPE(name) class 95th_Basic_Vest_##name## : JLTS_CloneVestHolster {\
 	author=95th Aux Team;\
 	displayName=[95th] Basic Vest;\
 	hiddenSelectionsTextures[]={\
@@ -33,22 +21,21 @@
 		Type = ##name##;\
 	};
 
-#define NEW_95TH_BASIC_VEST_NO_BOTTOM(name,baseclass) class 95th_Basic_Vest_##name## : ##baseclass## {\
-	author=95th Aux Team;\
-	displayName=[95th] Basic Vest;\
-	hiddenSelectionsTextures[]={\
-		,\
-		\95th_Vests\Data\Basic\##name##_Vest_Top.paa\
-	};\
-	class XtdGearInfo {\
-		model = 95th_Basic_Vests;\
-		Type = ##name##;\
-	};
-
 #define NEW_95TH_VEST_RECON_TYPE(name) class 95th_Basic_Vest_##name## : JLTS_CloneVestRecon {\
 	author=95th Aux Team;\
 	displayName=[95th] Basic Vest;\
 	hiddenSelectionsTextures[]={\95th_Vests\Data\Basic\##name##_Vest_Recon.paa};\
+	class ItemInfo: VestItem {\
+		uniformModel=\MRC\JLTS\characters\CloneArmor\CloneVestRecon.p3d;\
+		containerClass = Supply190;\
+		hiddenSelections[]= { camo1 };\
+		mass=80;\
+		class HitpointsProtectionInfo {\
+			class Chest { HitpointName=HitChest; armor=6; PassThrough=0.30000001; };\
+			class Legs { hitpointName=HitLegs; armor=6; passThrough=0.30000001; };\
+			class Arms { hitpointName=HitArms; armor=4; passThrough=0.30000001; };\
+		};\
+	};\
 	class XtdGearInfo {\
 		model = 95th_Basic_Vests;\
 		Type = ##name##;\
@@ -58,8 +45,41 @@
 	author=95th Aux Team;\
 	displayName=[95th] Basic Vest;\
 	hiddenSelectionsTextures[]={\95th_Vests\Data\Basic\##name##_Vest_Suspender.paa};\
+	class ItemInfo: VestItem {\
+		uniformModel=\MRC\JLTS\characters\CloneArmor\CloneVestSuspender.p3d;\
+		containerClass = Supply190;\
+		hiddenSelections[]= { camo1 };\
+		mass=80;\
+		class HitpointsProtectionInfo {\
+			class Chest { HitpointName=HitChest; armor=6; PassThrough=0.30000001; };\
+			class Legs { hitpointName=HitLegs; armor=6; passThrough=0.30000001; };\
+			class Arms { hitpointName=HitArms; armor=4; passThrough=0.30000001; };\
+		};\
+	};\
 	class XtdGearInfo {\
 		model = 95th_Basic_Vests;\
 		Type = ##name##;\
 	};
-	
+
+#define NEW_95TH_VEST_AIRBORNE_TYPE(name) class 95th_Basic_Vest_##name## : JLTS_CloneVestAirborne {\
+	author=95th Aux Team;\
+	displayName=[95th] Basic Vest;\
+	hiddenSelectionsTextures[]={\
+		,\
+		\95th_Vests\Data\Basic\##name##_Vest_Top.paa\
+	};\
+	class ItemInfo: VestItem {\
+		uniformModel=\MRC\JLTS\characters\CloneArmor\CloneVestAirborne.p3d;\
+		containerClass = Supply190;\
+		hiddenSelections[]= { camo1 };\
+		mass=80;\
+		class HitpointsProtectionInfo {\
+			class Chest { HitpointName=HitChest; armor=6; PassThrough=0.30000001; };\
+			class Legs { hitpointName=HitLegs; armor=6; passThrough=0.30000001; };\
+			class Arms { hitpointName=HitArms; armor=4; passThrough=0.30000001; };\
+		};\
+	};\
+	class XtdGearInfo {\
+		model = 95th_Basic_Vests;\
+		Type = ##name##;\
+	};
