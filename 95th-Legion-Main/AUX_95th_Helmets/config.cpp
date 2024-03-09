c243236d488e5337a897bcb6c85a5d6f91dcabcc
Taun-Taun <138324901+Taun-Taun@users.noreply.github.com>
2024-03-09 13:12:38 +0000

    Titan Helmet texture + Code

--- 95th-Legion-Main/AUX_95th_Helmets/config.cpp
+++ 95th-Legion-Main/AUX_95th_Helmets/config.cpp
@@ -89,11 +89,12 @@ class XtdGearModels {
 			class Player {
 				alwaysSelectable = 0;
 				labels = "Player";
-				values[] = {"Hypo", "Neary", "Smokery","Kev"};
+				values[] = {"Hypo", "Neary", "Smokery","Kev","Titan"};
 				class Hypo { label = "Hypo"; description = "Hypo's Helmet"; };
 				class Neary { label = "Neary"; description = "Neary's Helmet"; };
 				class Smokery { label = "Smokery"; description = "Smokery's Helmet"; };
 				class Kev { label = "Kev"; description = "Kev's Helmet"; };
+				class Titan { label = "Titan"; description = "Titan's Helmet"; };
 			};
 		};
 
