85108ac99c347a11786c47cc25e087b1a7165757
Turner605 <32025166+Turner605@users.noreply.github.com>
2024-04-01 23:25:41 +0100

    Splash stuff

--- 95th-Legion-Main/95th_P1_Units/config.cpp
+++ 95th-Legion-Main/95th_P1_Units/config.cpp
@@ -35,6 +35,8 @@ class CfgPatches {
 			"95th_Rifleman_Unit_Revan",
 			"95th_Rifleman_Unit_Sand",
 			"95th_Rifleman_Unit_Kev",
+			"95th_Rifleman_Unit_Sythe",
+			"95th_Rifleman_Unit_Splash",
 
 			"95th_MC_Unit_Zeus",
 			"95th_MC_Unit_Turner",
@@ -121,7 +123,7 @@ class XtdGearModels {
 			class Type {
 				alwaysSelectable = 1;
 				labels = "Type";
-				values[] = {"Volt","Jinter","Bretek","Commie","Smaug","Smokery","Manila","Havoc","MadHatter","Cheezy","Revan","Sand","Kev","Sythe"};
+				values[] = {"Volt","Jinter","Bretek","Commie","Smaug","Smokery","Manila","Havoc","MadHatter","Cheezy","Revan","Sand","Kev","Sythe","Splash"};
 				class Volt { label = "Volt"; description = "Volt's Uniform"; };
 				class Jinter { label = "Jinter"; description = "Jinter's Uniform"; };
 				class Bretek { label = "Bretek"; description = "Bretek's Uniform"; };
@@ -136,6 +138,7 @@ class XtdGearModels {
 				class Sand { label = "Sand"; description = "Sand's Uniform"; };
 				class Kev { label = "Kev"; description = "Kev's Uniform"; };
 				class Sythe { label = "Sythe"; description = "Sythe's Uniform"; };
+				class Splash { label = "Splash"; description = "Splash's Uniform"; };
 			};
 		};
 
@@ -260,6 +263,7 @@ class cfgWeapons {
 	NEW_95TH_CUSTOM_P1_UNIFORM(Sand,Sand)};
 	NEW_95TH_CUSTOM_P1_UNIFORM(Kev,Kev)};
 	NEW_95TH_CUSTOM_P1_UNIFORM(Sythe,Sythe)};
+	NEW_95TH_CUSTOM_P1_UNIFORM(Splash,Splash)};
 
 	NEW_95TH_MC_UNIFORM(Zeus,Zeus)};
 	NEW_95TH_MC_UNIFORM(Turner,Turner)};
@@ -291,13 +295,14 @@ class CfgVehicles {
 	NEW_95TH_RIFLEMAN_UNIT(Smaug,Smaug,Trooper)};
 	NEW_95TH_RIFLEMAN_UNIT(Smokery,Smokery,Trooper)};
 	NEW_95TH_RIFLEMAN_UNIT(Manila,Manila,Trooper)};
-	NEW_95TH_RIFLEMAN_UNIT(Havoc,Havoc,Zeus)};
+	NEW_95TH_RIFLEMAN_UNIT(Havoc,Havoc,Lieutenant)};
 	NEW_95TH_RIFLEMAN_UNIT(MadHatter,MadHatter,Veteran)};
 	NEW_95TH_RIFLEMAN_UNIT(Cheezy,Cheezy,Trooper)};
 	NEW_95TH_RIFLEMAN_UNIT(Revan,Revan,Trooper)};
 	NEW_95TH_RIFLEMAN_UNIT(Sand,Sand,Sergeant)};
 	NEW_95TH_RIFLEMAN_UNIT(Kev,Kev,Trooper)};
 	NEW_95TH_RIFLEMAN_UNIT(Sythe,Sythe,Sergeant)};
+	NEW_95TH_RIFLEMAN_UNIT(Splash,Splash,Zeus)};
 
 	// Uniform Class Name, Helmet Name, Vest Name
 	NEW_95TH_MC_UNIT(Zeus,Veteran,Zeus)};
