1deef389712d705e76e5f79cfed641be67047263
Taun-Taun <138324901+Taun-Taun@users.noreply.github.com>
2024-04-29 16:25:49 +0100

    Boom Code

--- 95th-Legion-Main/AUX_95th_Helmets/config.cpp
+++ 95th-Legion-Main/AUX_95th_Helmets/config.cpp
@@ -88,13 +88,14 @@ class XtdGearModels {
 			class Player {
 				alwaysSelectable = 0;
 				labels = "Player";
-				values[] = {"Hypo", "Neary", "Smokery","Kev", "Titan", "Smaug"};
+				values[] = {"Hypo", "Neary", "Smokery","Kev", "Titan", "Smaug", "Boom"};
 				class Hypo { label = "Hypo"; description = "Hypo's Helmet"; };
 				class Neary { label = "Neary"; description = "Neary's Helmet"; };
 				class Smokery { label = "Smokery"; description = "Smokery's Helmet"; };
 				class Kev { label = "Kev"; description = "Kev's Helmet"; };
 				class Titan { label = "Titan"; description = "Titan's Helmet"; };
 				class Smaug { label = "Smaug"; description = "Smaug's Helmet"; };
+				class Boom { label = "Boom"; description = "Boom's Helmet"; };
 			};
 		};
 
