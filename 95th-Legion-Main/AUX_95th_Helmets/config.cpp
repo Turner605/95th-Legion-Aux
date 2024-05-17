47eaebe0f66546e53d550890d40287b10898a730
Taun-Taun <138324901+Taun-Taun@users.noreply.github.com>
2024-05-17 17:41:55 +0100

    Mermaid Code

--- 95th-Legion-Main/AUX_95th_Helmets/config.cpp
+++ 95th-Legion-Main/AUX_95th_Helmets/config.cpp
@@ -71,7 +71,7 @@ class XtdGearModels {
 			class Player {
 				alwaysSelectable = 0;
 				labels = "Player";
-				values[] = {"Volt","Sand", "Manila", "Osprey", "Chimera", "King", "Lisek"};
+				values[] = {"Volt","Sand", "Manila", "Osprey", "Chimera", "King", "Lisek", "Mermaid"};
 				class Volt { label = "Volt"; description = "Volt's Helmet"; };
 				class Sand { label = "Sand"; description = "Sand's Helmet"; };
 				class Manila { label = "Manila"; description = "Manila's Helmet"; };
@@ -79,6 +79,7 @@ class XtdGearModels {
 				class Chimera { label = "Chimera"; description = "Chimera's Helmet"; };
 				class King { label = "King"; description = "King's Helmet"; };
 				class Lisek { label = "Lisek"; description = "Lisek's Helmet"; };
+				class Mermaid { label = "Mermaid"; description = "Mermaid's Helmet"; };
 			};
 		};
 
