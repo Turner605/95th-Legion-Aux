42d6a8edee0d3dc6ee60e7152d8f14246140c75a
QueenJolene <joliensermon@hotmail.nl>
2024-05-20 16:51:54 +0200

    Foley armour added

--- 95th-Legion-Main/AUX_95th_Characters/config.cpp
+++ 95th-Legion-Main/AUX_95th_Characters/config.cpp
@@ -34,6 +34,7 @@ class CfgPatches {
 			"AUX_95th_Unit_P1_Basic_Solvest",
 			"AUX_95th_Unit_P1_Basic_Dilanza",
 			"AUX_95th_Unit_P1_Basic_Hypo",
+			"AUX_95th_Unit_P1_Basic_Foley",
 
 			"AUX_95th_Unit_P1_Cold_Assault_Trooper",
 			"AUX_95th_Unit_Commando_Trooper",
