21bb5763e3af60333a915ad01c485914a87b33d7
Turner605 <32025166+Turner605@users.noreply.github.com>
2024-06-13 19:55:28 +0100

    Changes to BX's

---
+++ 95th-Legion-Main/AUX_95th_CIS_Forces/Units/BX/Shotgun.hpp
@@ -0,0 +1,23 @@
+class AUX_95th_Droid_BX_Unit_Shotgun : lsd_cis_bxdroid_specops {
+	displayName="BX CQB (SB-B3)";
+	editorCategory="AUX_95th_CIS_Forces";
+	editorSubcategory="AUX_95th_CIS_Forces_Droids_BX";
+	faction="AUX_95th_CIS";
+	facewear="";
+	identityTypes[]={"LanguageENG_F","Head_LSD_BX"};
+	AUX_95th_Has_Commando_Jump=1;
+	items[]={"ls_cis_bxCommando_vest","SWLB_comlink_droid","JLTS_NVG_droid_chip_2","ItemGPS","ItemMap","ItemWatch","ItemCompass"};
+	linkedItems[]={"ls_cis_bxCommando_vest","SWLB_comlink_droid","JLTS_NVG_droid_chip_2","ItemGPS","ItemMap","ItemWatch","ItemCompass"};
+	respawnLinkedItems[]={"ls_cis_bxCommando_vest","SWLB_comlink_droid","JLTS_NVG_droid_chip_2","ItemGPS","ItemMap","ItemWatch","ItemCompass"};
+	weapons[]={"JLTS_SBB3","WBK_Dutch_Vibro","Throw","Put"};
+	respawnWeapons[]={"JLTS_SBB3","WBK_Dutch_Vibro","Throw","Put"};
+	magazines[]={
+		"JLTS_SBB3_mag","JLTS_SBB3_mag","JLTS_SBB3_mag","JLTS_SBB3_mag","JLTS_SBB3_mag","JLTS_SBB3_mag",
+		"DSS_Magazine_Grenade_Flashbang","DSS_Magazine_Grenade_Flashbang"
+	};
+	respawnMagazines[]={
+		"JLTS_SBB3_mag","JLTS_SBB3_mag","JLTS_SBB3_mag","JLTS_SBB3_mag","JLTS_SBB3_mag","JLTS_SBB3_mag",
+		"DSS_Magazine_Grenade_Flashbang","DSS_Magazine_Grenade_Flashbang"
+	};
+	uniformClass="lsd_cis_bxDroid_uniform";
+};
---
+++ 95th-Legion-Main/AUX_95th_CIS_Forces/Units/BX/Sniper.hpp
@@ -0,0 +1,23 @@
+class AUX_95th_Droid_BX_Unit_Sniper : lsd_cis_bxdroid_specops {
+	displayName="BX Sniper (E-5S)";
+	editorCategory="AUX_95th_CIS_Forces";
+	editorSubcategory="AUX_95th_CIS_Forces_Droids_BX";
+	faction="AUX_95th_CIS";
+	facewear="";
+	identityTypes[]={"LanguageENG_F","Head_LSD_BX"};
+	AUX_95th_Has_Commando_Jump=1;
+	items[]={"ls_cis_bxCommando_vest","SWLB_comlink_droid","JLTS_NVG_droid_chip_2","ItemGPS","ItemMap","ItemWatch","ItemCompass"};
+	linkedItems[]={"ls_cis_bxCommando_vest","SWLB_comlink_droid","JLTS_NVG_droid_chip_2","ItemGPS","ItemMap","ItemWatch","ItemCompass"};
+	respawnLinkedItems[]={"ls_cis_bxCommando_vest","SWLB_comlink_droid","JLTS_NVG_droid_chip_2","ItemGPS","ItemMap","ItemWatch","ItemCompass"};
+	weapons[]={"JLTS_E5S","WBK_Dutch_Vibro","Throw","Put"};
+	respawnWeapons[]={"JLTS_E5S","WBK_Dutch_Vibro","Throw","Put"};
+	magazines[]={
+		"JLTS_E5S_mag","JLTS_E5S_mag","JLTS_E5S_mag","JLTS_E5S_mag","JLTS_E5S_mag","JLTS_E5S_mag",
+		"DSS_Magazine_Grenade_Flashbang","DSS_Magazine_Grenade_Flashbang"
+	};
+	respawnMagazines[]={
+		"JLTS_E5S_mag","JLTS_E5S_mag","JLTS_E5S_mag","JLTS_E5S_mag","JLTS_E5S_mag","JLTS_E5S_mag",
+		"DSS_Magazine_Grenade_Flashbang","DSS_Magazine_Grenade_Flashbang"
+	};
+	uniformClass="lsd_cis_bxDroid_uniform";
+};
