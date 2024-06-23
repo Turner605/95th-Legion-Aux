b110c87d4e560828ccf56195694d579228b94c83
Taun-Taun <138324901+Taun-Taun@users.noreply.github.com>
2024-06-23 13:31:01 +0100

    Taun Custom Helmet

--- 95th-Legion-Main/AUX_95th_Helmets/config.cpp
+++ 95th-Legion-Main/AUX_95th_Helmets/config.cpp
@@ -108,11 +108,12 @@ class XtdGearModels {
 			class Player {
 				alwaysSelectable = 0;
 				labels = "Player";
-				values[] = {"Foley", "Mobius", "Taylor", "Stitch"};
+				values[] = {"Foley", "Mobius", "Taylor", "Stitch", "Taun"};
 				class Foley { label = "Foley"; description = "Foley's Helmet"; };
 				class Mobius { label = "Mobius"; description = "Mobius' Helmet"; };
 				class Taylor { label = "Taylor"; description = "Taylor's Helmet"; };
 				class Stitch { label = "Stitch"; description = "Stitch's Helmet"; };
+				class Taun { label = "Taun"; description = "Taun's Helmet"; };
 			};
 		};
 
