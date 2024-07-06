e2ccce0bfac65b5d046fddc1edc87d9d7c249a8d
Turner605 <32025166+Turner605@users.noreply.github.com>
2024-07-06 22:04:22 +0100

    Updating curator scope

--- 95th-Legion-Main/AUX_95th_Objects/Misc/2D Field Commander Kit.hpp
+++ 95th-Legion-Main/AUX_95th_Objects/Misc/2D Field Commander Kit.hpp
@@ -2,4 +2,5 @@ class AUX_95th_Misc_2DFCK : DSS_Objects_Misc_Cardboard_Target {
 	displayName="2D Field Commander Kit";
 	editorCategory="AUX_95th_Objects";
 	editorSubcategory="AUX_95th_Misc";
+	scopecurator=2;
 };
