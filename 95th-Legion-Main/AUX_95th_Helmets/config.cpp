15d96e9b92bd05b9a1cb9fa2545aad5a878fe0f2
QueenJolene <joliensermon@hotmail.nl>
2024-07-07 23:57:00 +0200

    Axe helmet added

--- 95th-Legion-Main/AUX_95th_Helmets/config.cpp
+++ 95th-Legion-Main/AUX_95th_Helmets/config.cpp
@@ -168,9 +168,10 @@ class XtdGearModels {
 			class Player {
 				alwaysSelectable = 0;
 				labels = "Player";
-				values[] = {"Hypo", "Dilanza"};
+				values[] = {"Hypo", "Dilanza", "Axe"};
 				class Hypo { label = "Hypo"; description = "Hypo's Helmet"; };
 				class Dilanza { label = "Dilanza"; description = "Dilanza's Helmet"; };
+				class Axe { label = "Axe"; description = "Axe's Helmet"; };
 			};
 		};
 
