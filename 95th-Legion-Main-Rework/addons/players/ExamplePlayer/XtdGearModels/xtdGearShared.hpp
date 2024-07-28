4cd2af88b78876be558641f9659e08ef8ebc00b3
Turner605 <32025166+Turner605@users.noreply.github.com>
2024-07-28 16:42:33 +0100

    The rest of the helmets

--- 95th-Legion-Main-Rework/addons/players/ExamplePlayer/XtdGearModels/xtdGearShared.hpp
+++ 95th-Legion-Main-Rework/addons/players/ExamplePlayer/XtdGearModels/xtdGearShared.hpp
@@ -1,17 +1,25 @@
 author = "95th Aux Team";
-options[] = {"Type", "Player"};
+options[] = {"Phase", "Type", "Player"};
+
+class Phase {
+	alwaysSelectable = 1;
+	labels = "Phase";
+	values[] = {"P1","P2"};
+	class P1 { label = "P1"; description = "Phase 1"; };
+	class P2 { label = "P2"; description = "Phase 2"; };
+};
 
 class Type {
-	alwaysSelectable = 1;
+	alwaysSelectable = 0;
 	labels = "Type";
-	values[] = {"P1_Basic","P1_ARF","P1_Engineer","P1_Pilot","P1_SpecOps","P1_Hazard","P1_ColdAssault","P1_Commando","P2_Airborne"};
+	values[] = {"P1_Basic", "P1_ARF", "P1_Engineer", "P1_Pilot", "P1_SpecOps", "P1_Hazard", "P1_Cold_Assault", "P1_Commando", "P2_Airborne"};
 	class P1_Basic { label = "P1 Basic"; description = "Basic P1 Helmet"; };
 	class P1_ARF { label = "ARF"; description = "ARF Helmet"; };
 	class P1_Engineer { label = "Engineer"; description = "Engineer Helmet"; };
 	class P1_Pilot { label = "P1 Pilot"; description = "P1 Pilot Helmet"; };
 	class P1_SpecOps { label = "SpecOps"; description = "SpecOps Helmet"; };
 	class P1_Hazard { label = "Hazard"; description = "Hazard Helmet"; };
-	class P1_ColdAssault { label = "Cold Assault"; description = "Cold Assault Helmet"; };
+	class P1_Cold_Assault { label = "Cold Assault"; description = "Cold Assault Helmet"; };
 	class P1_Commando { label = "Commando"; description = "Commando Helmet"; };
-	class Airborne { label = "Airborne"; description = "Airborne Helmet"; };
+	class P2_Airborne { label = "Airborne"; description = "Airborne Helmet"; };
 };
