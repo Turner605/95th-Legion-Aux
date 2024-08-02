42c7a634519aa7eb1df2ace3a6ccff6996d21865
Turner605 <32025166+Turner605@users.noreply.github.com>
2024-08-02 19:02:44 +0100

    Deprecated old terminal

--- 95th-Legion-Main/AUX_95th_Objects/GroundUtil/Vehicles.hpp
+++ 95th-Legion-Main/AUX_95th_Objects/GroundUtil/Vehicles.hpp
@@ -3,60 +3,10 @@ class AUX_95th_GroundUtil_Vehicles : 3AS_Terminal_1_Prop {
 	editorCategory="AUX_95th_Objects";
 	editorSubcategory="AUX_95th_Ground_Utilities";
 	class UserActions {
-		class SpawnFirehawke {
+		class SpawnDeprecated {
 			priority = 10; radius = 10; position = "camera"; showWindow = 0; onlyForPlayer = 0; shortcut = ""; condition = "alive this;";
-			displayName = "Spawn [95th] Firehawke";
-			statement = "[this, player, 'AUX_95th_Firehawke'] spawn AUX_95th_fnc_handleGroundSpawned;"; 
-		};
-		class SpawnTX130 {
-			priority = 10; radius = 10; position = "camera"; showWindow = 0; onlyForPlayer = 0; shortcut = ""; condition = "alive this;";
-			displayName = "Spawn [95th] TX-130";
-			statement = "[this, player, 'AUX_95th_TX_130'] spawn AUX_95th_fnc_handleGroundSpawned;"; 
-		};
-		class SpawnTX130Shield {
-			priority = 10; radius = 10; position = "camera"; showWindow = 0; onlyForPlayer = 0; shortcut = ""; condition = "alive this;";
-			displayName = "Spawn [95th] TX-130 (Shield)";
-			statement = "[this, player, 'AUX_95th_TX_130_Shield'] spawn AUX_95th_fnc_handleGroundSpawned;"; 
-		};
-		// class SpawnTX130Strafing {
-		// 	priority = 10; radius = 10; position = "camera"; showWindow = 0; onlyForPlayer = 0; shortcut = ""; condition = "alive this;";
-		// 	displayName = "Spawn [3AS] TX-130 (Strafing)";
-		// 	statement = "[this, player, '3as_saber_m1_strafe'] spawn AUX_95th_fnc_handleGroundSpawned;"; 
-		// };
-		class Spawn95thRX200 {
-			priority = 10; radius = 10; position = "camera"; showWindow = 0; onlyForPlayer = 0; shortcut = ""; condition = "alive this;";
-			displayName = "Spawn [95th] RX-200";
-			statement = "[this, player, 'AUX_95th_RX200'] spawn AUX_95th_fnc_handleGroundSpawned;"; 
-		};
-		class Spawn3ASATTE {
-			priority = 10; radius = 10; position = "camera"; showWindow = 0; onlyForPlayer = 0; shortcut = ""; condition = "alive this;";
-			displayName = "Spawn [95th] AT-TE";
-			statement = "[this, player, 'AUX_95th_ATTE'] spawn AUX_95th_fnc_handleGroundSpawned;"; 
-		};
-		class SpawnSwampSpeeder {
-			priority = 10; radius = 10; position = "camera"; showWindow = 0; onlyForPlayer = 0; shortcut = ""; condition = "alive this;";
-			displayName = "Spawn [95th] Swamp Speeder";
-			statement = "[this, player, 'AUX_95th_Swamp_Speeder'] spawn AUX_95th_fnc_handleGroundSpawned;"; 
-		};
-		class SpawnSwampSpeederTransport {
-			priority = 10; radius = 10; position = "camera"; showWindow = 0; onlyForPlayer = 0; shortcut = ""; condition = "alive this;";
-			displayName = "Spawn [95th] Swamp Speeder (Transport)";
-			statement = "[this, player, 'AUX_95th_Swamp_Speeder_Transport'] spawn AUX_95th_fnc_handleGroundSpawned;"; 
-		};
-		class SpawnCommandOutpost {
-			priority = 10; radius = 10; position = "camera"; showWindow = 0; onlyForPlayer = 0; shortcut = ""; condition = "alive this;";
-			displayName = "Spawn [95th] Command Post";
-			statement = "[this, player, 'AUX_95th_Command_Outpost'] spawn AUX_95th_fnc_handleGroundSpawned;"; 
-		};
-		class LoadATRT {
-			priority = 10; radius = 10; position = "camera"; showWindow = 0; onlyForPlayer = 0; shortcut = ""; condition = "alive this;";
-			displayName = "Load AT-RT Into LAAT";
-			statement = "[this] spawn AUX_95th_fnc_handleLoadATRT;"; 
-		};
-		class SpawnBantha {
-			priority = 10; radius = 10; position = "camera"; showWindow = 0; onlyForPlayer = 0; shortcut = ""; condition = "alive this;";
-			displayName = "Spawn [95th] Bantha";
-			statement = "[this, player, 'AUX_95th_Bantha'] spawn AUX_95th_fnc_handleGroundSpawned;"; 
+			displayName = "DEPRECATED";
+			statement = "systemChat 'THIS ASSET IS DEPRECATED';"; 
 		};
 	};
 };
