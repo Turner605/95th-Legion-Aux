3f82d4d9f48fbf19be4324b763ea6097ce60d044
Turner605 <32025166+Turner605@users.noreply.github.com>
2024-08-28 23:33:48 +0100

    Basic gear

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/unassigned.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/unassigned.hpp
@@ -1,8 +1,7 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Biedronka", "Havoc", "ExamplePlayer"};
+    values[] = {"Biedronka", "Havoc"};
     class Biedronka { label = "Biedronka"; description = "Biedronka Helmet"; };
     class Havoc { label = "Havoc"; description = "Havoc`s Helmet"; };
-    class ExamplePlayer { label = "ExamplePlayer"; description = "ExamplePlayer`s Helmet"; };
 };
---
+++ 95th-Legion-Main-Rework/addons/players/trooper/script_component.hpp
@@ -0,0 +1,53 @@
+#define COMPONENT players_trooper
+#define COMPONENT_BEAUTIFIED Players_Trooper
+#define PLAYER_NAME trooper
+#define PLAYER_NAME_BEAUTIFIED Trooper
+
+//OPTIONS: Unassigned, Basic, Fallen, Zeus, PLATOON_SQUAD (EG One_Seven)
+#define SQUAD_KEY Basic
+
+//OPTIONS: Unassigned, Basic, Fallen, Zeus, Ackley, Reek, .....
+#define SQUAD_NAME Basic
+
+// HELMET OPTIONS 0 (False), 1 (True)
+#define HELMET_P1_BASIC 1
+#define HELMET_P1_ENGINEER 1
+#define HELMET_P1_ARF 1
+#define HELMET_P1_PILOT 1
+#define HELMET_P1_SPECOPS 1
+#define HELMET_P1_HAZARD 1
+#define HELMET_P1_COLD_ASSAULT 1
+#define HELMET_P1_COMMANDO 1
+#define HELMET_P2_AIRBORNE 1
+
+// UNIFORM OPTIONS 0 (False), 1 (True)
+#define UNIFORM_P1_BASIC 1
+#define UNIFORM_P1_MC 1
+#define UNIFORM_P1_COLDASSAULT 1
+#define UNIFORM_P1_COMMANDO 1
+
+// BACKPACK OPTIONS 0 (False), 1 (True)
+#define BACKPACK_BELT_LIGHT 1
+#define BACKPACK_MINI_LIGHT 1
+#define BACKPACK_MICRO_LIGHT 1
+#define BACKPACK_RUCKSACK_LIGHT 1
+#define BACKPACK_RUCKSACK_HEAVY 1
+#define BACKPACK_RUCKSACK_MEDIC 1
+#define BACKPACK_RUCKSACK_ORDNANCE 1
+#define JETPACK_JT12_LIGHT 1
+#define JETPACK_JT12_MEDIC 1
+#define JETPACK_JT14_LIGHT 1
+
+// VEST OPTIONS 0 (False), 1 (True)
+#define VEST_STANDARD_SET 1
+#define VEST_ARC_SET 1
+#define VEST_OFFICER_SET 1
+#define VEST_COMMANDO_SET 1
+
+// DO NOT EDIT
+#define PARENTCOMPONENT shared
+#include "\z\AUX_95th\addons\main\script_mod.hpp"
+#include "\z\AUX_95th\addons\main\script_macros.hpp"
+
+//TODO:
+// Vests need replacing really 
