a9282028aebdc5aea5d91f75e5fb7ccb4de5344a
Turner605 <32025166+Turner605@users.noreply.github.com>
2024-08-29 21:01:26 +0100

    More helmet migration

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_four.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_four.hpp
@@ -1,10 +1,11 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Bretek", "Third", "Ratler", "York", "Night"};
-    class Bretek { label = "Bretek"; description = "Bretek`s Helmet"; };
-    class Third { label = "Third"; description = "Third`s Helmet"; };
-    class Ratler { label = "Ratler"; description = "Ratler`s Helmet"; };
-    class York { label = "York"; description = "York`s Helmet"; };
-    class Night { label = "Night"; description = "Night`s Helmet"; };
+    values[] = {"Bretek", "Third", "Ratler", "York", "Night", "Kaz"};
+    class Bretek { label = "Bretek"; description = "Bretek`s"; };
+    class Third { label = "Third"; description = "Third`s"; };
+    class Ratler { label = "Ratler"; description = "Ratler`s"; };
+    class York { label = "York"; description = "York`s"; };
+    class Night { label = "Night"; description = "Night`s"; };
+    class Kaz { label = "Night"; description = "Kaz`s"; };
 };
---
+++ 95th-Legion-Main-Rework/addons/players/madHatter/script_component.hpp
@@ -0,0 +1,54 @@
+#define COMPONENT players_madHatter
+#define COMPONENT_BEAUTIFIED Players_MadHatter
+#define PLAYER_NAME madHatter
+#define PLAYER_NAME_BEAUTIFIED MadHatter
+
+//OPTIONS: Unassigned, Basic, Fallen, Zeus, PLATOON_SQUAD (EG One_Seven)
+#define SQUAD_KEY Fallen
+
+//OPTIONS: Unassigned, Basic, Fallen, Zeus, Ackley, Reek, .....
+#define SQUAD_NAME Fallen
+
+// HELMET OPTIONS 0 (False), 1 (True)
+#define HELMET_P1_BASIC 1
+#define HELMET_P1_ENGINEER 0
+#define HELMET_P1_ARF 0
+#define HELMET_P1_PILOT 0
+#define HELMET_P1_SPECOPS 0
+#define HELMET_P1_HAZARD 0
+#define HELMET_P1_COLD_ASSAULT 0
+#define HELMET_P1_COMMANDO 0
+#define HELMET_P2_AIRBORNE 0
+
+// UNIFORM OPTIONS 0 (False), 1 (True)
+#define UNIFORM_P1_BASIC 0
+#define UNIFORM_P1_MC 0
+#define UNIFORM_P1_COLDASSAULT 0
+#define UNIFORM_P1_COMMANDO 0
+
+// BACKPACK OPTIONS 0 (False), 1 (True)
+#define BACKPACK_BELT_LIGHT 0
+#define BACKPACK_MINI_LIGHT 0
+#define BACKPACK_MICRO_LIGHT 0
+#define BACKPACK_RUCKSACK_LIGHT 0
+#define BACKPACK_RUCKSACK_HEAVY 0
+#define BACKPACK_RUCKSACK_MEDIC 0
+#define BACKPACK_RUCKSACK_ORDNANCE 0
+#define JETPACK_JT12_LIGHT 0
+#define JETPACK_JT12_MEDIC 0
+#define JETPACK_JT14_LIGHT 0
+#define BACKPACK_COMMANDO_HEAVY 0
+
+// VEST OPTIONS 0 (False), 1 (True)
+#define VEST_STANDARD_SET 0
+#define VEST_ARC_SET 0
+#define VEST_OFFICER_SET 0
+#define VEST_COMMANDO_SET 0
+
+// DO NOT EDIT
+#define PARENTCOMPONENT shared
+#include "\z\AUX_95th\addons\main\script_mod.hpp"
+#include "\z\AUX_95th\addons\main\script_macros.hpp"
+
+//TODO:
+// Vests need replacing really 
