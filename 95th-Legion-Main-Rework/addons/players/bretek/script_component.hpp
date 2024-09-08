ac76cf4761656eaba35d08da887b87b06c23250e
Taun-Taun <138324901+Taun-Taun@users.noreply.github.com>
2024-09-08 08:24:43 +0100

    Updating ARC capacity for Bretek

--- 95th-Legion-Main-Rework/addons/players/bretek/script_component.hpp
+++ 95th-Legion-Main-Rework/addons/players/bretek/script_component.hpp
@@ -41,7 +41,7 @@
 
 // VEST OPTIONS 0 (False), 1 (True)
 #define VEST_STANDARD_SET 0
-#define VEST_ARC_SET 0
+#define VEST_ARC_SET 1
 #define VEST_OFFICER_SET 1
 #define VEST_COMMANDO_SET 0
 
@@ -51,4 +51,4 @@
 #include "\z\AUX_95th\addons\main\script_macros.hpp"
 
 //TODO:
-// Vests need replacing really 
+// Vests need replacing really
