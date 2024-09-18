3695ec6591e1c1d9a7027409cdaedba9a319ec9a
Turner605 <32025166+Turner605@users.noreply.github.com>
2024-09-18 12:28:52 +0100

    dc15c migrated

---
+++ 95th-Legion-Main-Rework/addons/weapons/dc15c/script_component.hpp
@@ -0,0 +1,8 @@
+#define COMPONENT weapons_dc15c
+#define COMPONENT_BEAUTIFIED Weapons_DC15C
+#define WEAPON_NAME dc15c
+#define WEAPON_NAME_BEAUTIFIED DC15C
+
+#define PARENTCOMPONENT shared
+#include "\z\AUX_95th\addons\main\script_mod.hpp"
+#include "\z\AUX_95th\addons\main\script_macros.hpp"
