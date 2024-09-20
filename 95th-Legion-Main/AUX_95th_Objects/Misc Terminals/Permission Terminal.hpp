1ff8744b7abf43af3bb053194d8be5d9d89d49eb
Turner605 <32025166+Turner605@users.noreply.github.com>
2024-09-20 14:29:15 +0100

    Permission Terminal Improvements

--- 95th-Legion-Main/AUX_95th_Objects/Misc Terminals/Permission Terminal.hpp
+++ 95th-Legion-Main/AUX_95th_Objects/Misc Terminals/Permission Terminal.hpp
@@ -1,7 +1,11 @@
 class AUX_95th_Misc_Permission_Terminal : DSS_Permissions_Droid {
 	model="optre_bw_buildings\reserchbase\doorconsole\doorconsole.p3d";
-	hiddenSelectionsTextures[]= { "optre_bw_buildings\reserchbase\doorconsole\data\cor_terminal_grn.paa" };
 	displayName="Permission Terminal";
 	editorCategory="AUX_95th_Objects";
 	editorSubcategory="AUX_95th_Misc_Terminals";
+	hiddenSelections[] = {"Screens","Lightbar"};
+	hiddenSelectionsTextures[] = {
+		"\AUX_95th_Objects\Data\Terminals\Permissions.paa", 
+		"#(argb,8,8,3)color(1,0,0,1.0,co)"
+	};
 };
