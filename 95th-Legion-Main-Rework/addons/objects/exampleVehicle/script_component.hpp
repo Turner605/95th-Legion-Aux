8fc71f261fb454f2d16f65f901460361a9e711e5
Turner605 <32025166+Turner605@users.noreply.github.com>
2024-09-26 15:41:05 +0100

    Objects folder

---
+++ 95th-Legion-Main-Rework/addons/objects/exampleVehicle/script_component.hpp
@@ -0,0 +1,8 @@
+#define COMPONENT objects_exampleObject
+#define COMPONENT_BEAUTIFIED Objects_ExampleObject
+#define OBJECT_NAME exampleObject
+#define OBJECT_NAME_BEAUTIFIED ExampleObject
+
+#define PARENTCOMPONENT shared
+#include "\z\AUX_95th\addons\main\script_mod.hpp"
+#include "\z\AUX_95th\addons\main\script_macros.hpp"
