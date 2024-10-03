f9e369c526c5cec86c04086894e52026e277e532
Taun-Taun <138324901+Taun-Taun@users.noreply.github.com>
2024-10-03 14:55:18 +0100

    Token + Ranger Helmets + code

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/zeus.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/zeus.hpp
@@ -1,9 +1,11 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Turner","Splash", "Commie", "Solvest"};
+    values[] = {"Turner","Splash", "Commie", "Solvest", "Ranger", "Token"};
     class Turner { label = "Turner"; description = "Turner`s Helmet"; };
     class Splash { label = "Splash"; description = "Splash`s Helmet"; };
     class Commie { label = "Commie"; description = "Commie`s Helmet"; };
     class Solvest { label = "Solvest"; description = "Solvest`s Helmet"; };
+    class Token { label = "Token"; description = "Token`s Helmet"; };
+    class Ranger { label = "Ranger"; description = "Ranger`s Helmet"; };
 };
