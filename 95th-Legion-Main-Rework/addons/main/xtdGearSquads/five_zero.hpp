816da3e3060fd3fb3782d96faea128a0f29f9a4e
Taun-Taun <138324901+Taun-Taun@users.noreply.github.com>
2024-10-14 23:41:10 +0100

    Hypo Engi code

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/five_zero.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/five_zero.hpp
@@ -1,8 +1,9 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Kong", "Revan", "Taun"};
+    values[] = {"Kong", "Revan", "Taun", "Hypo"};
     class Kong { label = "Kong"; description = "Kong`s Helmet"; };
     class Revan { label = "Revan"; description = "Revan`s Helmet"; };
     class Taun { label = "Taun"; description = "Taun`s"; };
+    class Hypo { label = "Hypo"; description = "Hypo`s"; };
 };
--- 95th-Legion-Main-Rework/addons/players/hypo/script_component.hpp
+++ 95th-Legion-Main-Rework/addons/players/hypo/script_component.hpp
@@ -4,11 +4,11 @@
 #define PLAYER_NAME_BEAUTIFIED Hypo
 
 //OPTIONS: Zeus, Fallen, Unassigned, Basic, One_Zero, One_One, One_Two, One_Three, One_Four, Five_Zero, Six_Zero, Seven_Zero
-#define SQUAD_KEY Seven_Zero
+#define SQUAD_KEY Five_Zero
 
 // HELMET OPTIONS 0 (False), 1 (True)
 #define HELMET_P1_BASIC 1
-#define HELMET_P1_ENGINEER 0
+#define HELMET_P1_ENGINEER 1
 #define HELMET_P1_ARF 0
 #define HELMET_P1_PILOT 0
 #define HELMET_P1_SPECOPS 0
@@ -48,4 +48,4 @@
 #include "\z\AUX_95th\addons\main\script_macros.hpp"
 
 //TODO:
-// Vests need replacing really 
+// Vests need replacing really
