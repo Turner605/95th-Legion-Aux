aba2c3325c2628e3c7eeedca806f9b40106a34fa
QueenJolene <joliensermon@hotmail.nl>
2025-01-12 18:17:57 +0100

    Helmet Mute added

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_three.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_three.hpp
@@ -1,9 +1,10 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Gear", "Death", "MoonShine", "Sparkie"};
+    values[] = {"Gear", "Death", "MoonShine", "Sparkie", "Mute"};
     class Gear { label = "Gear"; description = "Gear`s"; };
     class Death { label = "Death"; description = "Death`s"; };
     class MoonShine { label = "MoonShine"; description = "MoonShine`s"; };
     class Sparkie { label = "Sparkie"; description = "Sparkie`s"; };
+    class Mute { label = "Mute"; description = "Mute`s"; };
 };
