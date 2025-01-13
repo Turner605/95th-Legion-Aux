70c73fab0b0b3b73905f36d53a100b6a95809d98
QueenJolene <joliensermon@hotmail.nl>
2025-01-13 15:09:17 +0100

    Helmet Talisman added

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/six_zero.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/six_zero.hpp
@@ -1,8 +1,9 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Edge", "Neron", "Styx"};
+    values[] = {"Edge", "Neron", "Styx", "Talisman"};
     class Edge { label = "Edge"; description = "Edge`s Helmet"; };
     class Neron { label = "Neron"; description = "Neron`s Helmet"; };
-        class Styx { label = "Styx"; description = "Styx`s"; };
+    class Styx { label = "Styx"; description = "Styx`s"; };
+    class Talisman { label = "Talisman"; description = "Talisman`s"; };
 };
