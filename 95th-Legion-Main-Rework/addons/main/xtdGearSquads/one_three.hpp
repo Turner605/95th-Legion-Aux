5fd0954a232bc870fcd31b9452df056ff0f3a0d2
QueenJolene <joliensermon@hotmail.nl>
2025-01-19 02:04:57 +0100

    Jack backpack added

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_three.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_three.hpp
@@ -1,10 +1,11 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Gear", "Death", "MoonShine", "Sparkie", "Mute"};
+    values[] = {"Gear", "Death", "MoonShine", "Sparkie", "Mute", "Jack"};
     class Gear { label = "Gear"; description = "Gear`s"; };
     class Death { label = "Death"; description = "Death`s"; };
     class MoonShine { label = "MoonShine"; description = "MoonShine`s"; };
     class Sparkie { label = "Sparkie"; description = "Sparkie`s"; };
     class Mute { label = "Mute"; description = "Mute`s"; };
+    class Jack { label = "Jack"; description = "Jack`s"; };
 };
