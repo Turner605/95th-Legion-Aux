69f684ef971c3612a3416786cea9040e7c9ca9a6
QueenJolene <joliensermon@hotmail.nl>
2025-02-13 20:52:36 +0100

    Helmet Elusive added

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_three.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_three.hpp
@@ -1,11 +1,12 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Gear", "Death", "MoonShine", "Sparkie", "Mute", "Jack"};
+    values[] = {"Gear", "Death", "MoonShine", "Sparkie", "Mute", "Jack", "Elusive"};
     class Gear { label = "Gear"; description = "Gear`s"; };
     class Death { label = "Death"; description = "Death`s"; };
     class MoonShine { label = "MoonShine"; description = "MoonShine`s"; };
     class Sparkie { label = "Sparkie"; description = "Sparkie`s"; };
     class Mute { label = "Mute"; description = "Mute`s"; };
     class Jack { label = "Jack"; description = "Jack`s"; };
+    class Elusive { label = "Elusive"; description = "Elusive`s"; };
 };
