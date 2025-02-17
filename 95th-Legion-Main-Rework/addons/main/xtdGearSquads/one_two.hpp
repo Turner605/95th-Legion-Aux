c4c249c32d67b026248b9ce422b5065e5dd83fd1
QueenJolene <joliensermon@hotmail.nl>
2025-02-17 19:36:01 +0100

    Dagger jumppack added

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_two.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_two.hpp
@@ -1,7 +1,7 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Neary", "Smokery", "Kev", "Titan", "Smaug", "Boom", "Blue"};
+    values[] = {"Neary", "Smokery", "Kev", "Titan", "Smaug", "Boom", "Blue", "Dagger"};
     class Neary { label = "Neary"; description = "Neary`s"; };
     class Smokery { label = "Smokery"; description = "Smokery`s"; };
     class Kev { label = "Kev"; description = "Kev`s"; };
@@ -9,4 +9,5 @@ class Player {
     class Smaug { label = "Smaug"; description = "Smaug`s"; };
     class Boom { label = "Boom"; description = "Boom`s"; };
     class Blue { label = "Blue"; description = "Blue`s"; };
+    class Dagger { label = "Dagger"; description = "Dagger`s"; };
 };
