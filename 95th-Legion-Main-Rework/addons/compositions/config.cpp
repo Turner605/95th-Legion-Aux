aeb6f3f4fd0ab61bfd9e7f6ac24ba341b8d0bb1f
Turner605 <32025166+Turner605@users.noreply.github.com>
2025-03-03 17:12:54 +0000

    New composition

--- 95th-Legion-Main-Rework/addons/compositions/config.cpp
+++ 95th-Legion-Main-Rework/addons/compositions/config.cpp
@@ -13,24 +13,42 @@ class CfgPatches {
 };
 
 class CfgEditorCategories {
-	class AUX_95th_Legion_Compositions_Blufor {
-		displayName = "[95th] Blufor";
+	class AUX_95th_Legion_Compositions_GAR {
+		displayName = "[95th] GAR";
+	};
+
+	class AUX_95th_Legion_Compositions_CIS {
+		displayName = "[95th] CIS";
 	};
 };
 
 class CfgEditorSubcategories {
-	class AUX_95th_Legion_Compositions_Blufor_Bases {
-		displayName = "Bases";
+	class AUX_95th_Legion_Compositions_FOBs {
+		displayName = "FOBs";
+	};
+
+	class AUX_95th_Legion_Compositions_Checkpoints {
+		displayName = "Checkpoints";
 	};
 };
 
 class Cfg3DEN {
 	class Compositions {
 		class AUX_95th_FOB_Blackweb {
-			path = "z\AUX_95th\addons\compositions\blufor\fob blackweb";
+			path = "z\AUX_95th\addons\compositions\gar\fob blackweb";
 			side = 1; // 0 opfor, 1 blufor, 2 indfor, 3 civ, 8 Empty/Props
-			editorCategory = "AUX_95th_Legion_Compositions_Blufor";
-			editorSubcategory = "AUX_95th_Legion_Compositions_Blufor_Bases";
+			editorCategory = "AUX_95th_Legion_Compositions_GAR";
+			editorSubcategory = "AUX_95th_Legion_Compositions_FOBs";
+			displayName = "FOB Blackweb";
+			icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+			useSideColorOnIcon = 1;
+		};
+
+		class AUX_95th_Opfor_Checkpoint_A {
+			path = "z\AUX_95th\addons\compositions\cis\checkpoint_1";
+			side = 0; // 0 opfor, 1 blufor, 2 indfor, 3 civ, 8 Empty/Props
+			editorCategory = "AUX_95th_Legion_Compositions_CIS";
+			editorSubcategory = "AUX_95th_Legion_Compositions_Checkpoints";
 			displayName = "FOB Blackweb";
 			icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
 			useSideColorOnIcon = 1;
