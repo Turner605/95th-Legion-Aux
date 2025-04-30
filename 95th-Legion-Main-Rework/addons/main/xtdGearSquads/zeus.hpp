199a1d746b1289d763b96e9f8cf46cd9b26a4e68
QueenJolene <joliensermon@hotmail.nl>
2025-04-30 20:20:34 +0200

    Move Death
    Moved from unassigned to zeus

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/zeus.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/zeus.hpp
@@ -1,11 +1,12 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Turner","Splash", "Commie", "Solvest", "Ranger", "Axe"};
+    values[] = {"Turner","Splash", "Commie", "Solvest", "Ranger", "Axe", "Death"};
     class Turner { label = "Turner"; description = "Turner`s Helmet"; };
     class Splash { label = "Splash"; description = "Splash`s Helmet"; };
     class Commie { label = "Commie"; description = "Commie`s Helmet"; };
     class Solvest { label = "Solvest"; description = "Solvest`s Helmet"; };
     class Ranger { label = "Ranger"; description = "Ranger`s Helmet"; };
     class Axe { label = "Axe"; description = "Axe`s Helmet"; };
+    class Death { label = "Death"; description = "Death`s"; };
 };
