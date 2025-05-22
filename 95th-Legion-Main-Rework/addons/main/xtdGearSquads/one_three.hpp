e975076a70eb5637174b29e8cedcaf2dd9c2d7e8
QueenJolene <joliensermon@hotmail.nl>
2025-05-22 10:27:30 +0200

    Grunzit's helmet added

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_three.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_three.hpp
@@ -1,11 +1,12 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"MoonShine", "Sparkie", "Mute", "Jack", "Canada", "Charlie"};
+    values[] = {"MoonShine", "Sparkie", "Mute", "Jack", "Canada", "Charlie", "Grunzit"};
     class MoonShine { label = "MoonShine"; description = "MoonShine`s"; };
     class Sparkie { label = "Sparkie"; description = "Sparkie`s"; };
     class Mute { label = "Mute"; description = "Mute`s"; };
     class Jack { label = "Jack"; description = "Jack`s"; };
     class Canada { label = "Canada"; description = "Canada`s"; };
     class Charlie { label = "Charlie"; description = "Charlie`s"; };
+    class Grunzit { label = "Grunzit"; description = "Grunzit`s"; };
 };
