10e67391e58c00e75e6a7997e7281b169c231598
Turner605 <32025166+Turner605@users.noreply.github.com>
2025-06-19 17:40:30 +0100

    Compositions

--- 95th-Legion-Main-Rework/addons/compositions/config.cpp
+++ 95th-Legion-Main-Rework/addons/compositions/config.cpp
@@ -14,11 +14,11 @@ class CfgPatches {
 
 class CfgEditorCategories {
 	class AUX_95th_Legion_Compositions_GAR {
-		displayName = "[95th] GAR";
+		displayName = "[95th] GAR Compositions";
 	};
 
 	class AUX_95th_Legion_Compositions_CIS {
-		displayName = "[95th] CIS";
+		displayName = "[95th] CIS Compositions";
 	};
 };
 
@@ -27,6 +27,7 @@ class CfgEditorSubcategories {
 	class AUX_95th_Legion_Compositions_Ships {displayName = "[95th] Ships";};
 	class AUX_95th_Legion_Compositions_Checkpoints {displayName = "[95th] Checkpoints";};
 	class AUX_95th_Legion_Compositions_AA {displayName = "[95th] AA";};
+	class AUX_95th_Legion_Compositions_Misc {displayName = "[95th] Misc";};
 };
 
 class Cfg3DEN {
@@ -51,22 +52,32 @@ class Cfg3DEN {
 			useSideColorOnIcon = 1;
 		};
 
-		class AUX_95th_Opfor_Checkpoint_A {
+		class AUX_95th_Opfor_Checkpoint_1 {
 			path = "z\AUX_95th\addons\compositions\cis\checkpoint_1";
 			side = 0; // 0 opfor, 1 blufor, 2 indfor, 3 civ, 8 Empty/Props
 			editorCategory = "AUX_95th_Legion_Compositions_CIS";
 			editorSubcategory = "AUX_95th_Legion_Compositions_Checkpoints";
-			displayName = "Checkpoint Alpha";
+			displayName = "Checkpoint 1";
 			icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
 			useSideColorOnIcon = 1;
 		};
 
-		class AUX_95th_Opfor_AA_Shielded_A {
+		class AUX_95th_Opfor_AA_Shielded_1 {
 			path = "z\AUX_95th\addons\compositions\cis\aa_shielded_1";
 			side = 0; // 0 opfor, 1 blufor, 2 indfor, 3 civ, 8 Empty/Props
 			editorCategory = "AUX_95th_Legion_Compositions_CIS";
 			editorSubcategory = "AUX_95th_Legion_Compositions_AA";
-			displayName = "AA (Shielded) Alpha";
+			displayName = "AA (Shielded) 1";
+			icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+			useSideColorOnIcon = 1;
+		};
+
+		class AUX_95th_Opfor_Protected_Shield_Gen_1 {
+			path = "z\AUX_95th\addons\compositions\cis\protected_shield_generator_1";
+			side = 0; // 0 opfor, 1 blufor, 2 indfor, 3 civ, 8 Empty/Props
+			editorCategory = "AUX_95th_Legion_Compositions_CIS";
+			editorSubcategory = "AUX_95th_Legion_Compositions_Misc";
+			displayName = "Protected Shield Generator";
 			icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
 			useSideColorOnIcon = 1;
 		};
