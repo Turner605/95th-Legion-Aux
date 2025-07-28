01d22f2ada93a1797bc8a80595b15fd5f047a231
Dagger95th <jacksmith69wbu@gmail.com>
2025-07-28 13:25:31 +0100

    Kev
    Moved from 1-2 to 5-0

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/five_zero.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/five_zero.hpp
@@ -1,8 +1,9 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Kong", "Revan", "Hypo"};
+    values[] = {"Kong", "Revan", "Hypo", "Kev"};
     class Kong { label = "Kong"; description = "Kong`s Helmet"; };
     class Revan { label = "Revan"; description = "Revan`s Helmet"; };
     class Hypo { label = "Hypo"; description = "Hypo`s"; };
+    class Kev { label = "Kev"; description = "Kev`s"; };
 };
