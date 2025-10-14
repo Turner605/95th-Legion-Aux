dc1e1536c81c8130db296f77c9d33ceac41a1dfe
Turner605 <32025166+Turner605@users.noreply.github.com>
2025-10-14 21:42:02 +0100

    Pushing up test shield object for lisek

--- 95th-Legion-Main-Rework/addons/objects/shields/config.cpp
+++ 95th-Legion-Main-Rework/addons/objects/shields/config.cpp
@@ -3,7 +3,7 @@
 class CfgPatches {
     class DOUBLES(PREFIX,COMPONENT) {
         name = COMPONENT_NAME;
-        units[] = {"AUX_95th_Medical_Shield", "AUX_95th_WBK_Combine_WalhammerShield_actual", "AUX_95th_Shield_Small"};
+        units[] = {"AUX_95th_Medical_Shield", "AUX_95th_WBK_Combine_WalhammerShield_actual", "AUX_95th_Shield_Small", "AUX_95th_Shield_Target_Test"};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"AUX_95th_main", "AUX_95th_objects_shared"};
@@ -38,6 +38,18 @@ class CfgVehicles {
         displayName="Shield (Small)";
         editorCategory="AUX_95th_Objects_Generic";
         editorSubcategory="AUX_95th_Objects_Shields";
+    };
+
+    class CBA_B_InvisibleTarget;
+    class AUX_95th_Shield_Target_Test : CBA_B_InvisibleTarget {
+        displayName="Shield (Test)";
+        editorCategory="AUX_95th_Objects_Generic";
+        editorSubcategory="AUX_95th_Objects_Shields";
+        model="3AS\3AS_Shield\SquadShield.p3d";
+        armor=999999;
+        sound="Shield";
+        scope=2;
+        scopecurator=2;
     };
 };
 
