da700dd255840e655a1668dfb395a90dd4610a38
QueenJolene <joliensermon@hotmail.nl>
2025-11-25 20:07:02 +0100

    Scootie helmet added

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_two.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_two.hpp
@@ -1,7 +1,7 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Titan", "Boom", "Blue", "Dagger", "Gum", "Ranger", "Pendragon"};
+    values[] = {"Titan", "Boom", "Blue", "Dagger", "Gum", "Ranger", "Pendragon", "Scootie"};
     class Titan { label = "Titan"; description = "Titan`s"; };
     class Boom { label = "Boom"; description = "Boom`s"; };
     class Blue { label = "Blue"; description = "Blue`s"; };
@@ -9,4 +9,5 @@ class Player {
     class Gum { label = "Gum"; description = "Gum`s"; };
     class Ranger { label = "Ranger"; description = "Ranger`s Helmet"; };
     class Pendragon { label = "Pendragon"; description = "Pendragon`s"; };
+    class Scootie { label = "Scootie"; description = "Scootie`s"; };
 };
