ebfcc4e7ad1184cc16943a8d49dbca745bc0be1b
QueenJolene <joliensermon@hotmail.nl>
2025-12-02 20:40:54 +0100

    Changes
    - Chip from Unassigned -> 1-4
    - Skylight from 1-4 -> Unassigned

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_four.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_four.hpp
@@ -1,11 +1,11 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"York", "Night", "Galvin", "Skylight", "Kaz", "Thor"};
+    values[] = {"York", "Night", "Galvin", "Kaz", "Thor", "Chip"};
     class York { label = "York"; description = "York`s"; };
     class Night { label = "Night"; description = "Night`s"; };
     class Galvin { label = "Galvin"; description = "Galvin`s"; };
-    class Skylight { label = "Skylight"; description = "Skylight`s"; };
     class Kaz { label = "Kaz"; description = "Kaz`s"; };
     class Thor { label = "Thor"; description = "Thor`s"; };
+    class Chip { label = "Chip"; description = "Chip`s"; };
 };
