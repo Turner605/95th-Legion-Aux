a7d28148842b0cbf4238be6ece3219e613e8eabb
Turner605 <32025166+Turner605@users.noreply.github.com>
2025-12-07 23:28:21 +0000

    Modifying B1's squads

--- 95th-Legion-Extras/addons/main/script_version.hpp
+++ 95th-Legion-Extras/addons/main/script_version.hpp
@@ -1,5 +1,5 @@
-#define MAJOR 2511
-#define MINOR 301
+#define MAJOR 2512
+#define MINOR 071
 #define PATCH 0
 #define BUILD 0
 
--- 95th-Legion-Extras/addons/units/groups/standardB1s.hpp
+++ 95th-Legion-Extras/addons/units/groups/standardB1s.hpp
@@ -22,14 +22,26 @@ class AUX_95th_Separatist_Army_B1_Standard {
     class AUX_95th_Groups_Droid_B1_AssaultTeam_AT {
         side = 0;
         faction = "AUX_95th_Separatist_Army";
-        name="B1 Assault Team (AT/AA)";
+        name="B1 Assault Team (AT)";
         class Unit0 {side = 0; vehicle = "JLTS_Droid_B1_Commander"; rank = "SERGEANT"; position[] = {0,0,0};};
         class Unit1 {side = 0; vehicle = "JLTS_Droid_B1_E5"; rank = "PRIVATE"; position[] = {5,0,0};};
-        class Unit2 {side = 0; vehicle = "JLTS_Droid_B1_Marine"; rank = "PRIVATE"; position[] = {5,5,0};};
-        class Unit3 {side = 0; vehicle = "JLTS_Droid_B1_AT"; rank = "PRIVATE"; position[] = {0,5,0};};
-        class Unit4 {side = 0; vehicle = "JLTS_Droid_B1_AR"; rank = "PRIVATE"; position[] = {-5,0,0};};
-        class Unit5 {side = 0; vehicle = "JLTS_Droid_B1_SBB3"; rank = "PRIVATE"; position[] = {-5,-5,0};};
-        class Unit6 {side = 0; vehicle = "JLTS_Droid_B1_Sniper"; rank = "PRIVATE"; position[] = {10,0,0};};
-        class Unit7 {side = 0; vehicle = "JLTS_Droid_B1_E5"; rank = "PRIVATE"; position[] = {10,5,0};};
+        class Unit2 {side = 0; vehicle = "JLTS_Droid_B1_AT"; rank = "PRIVATE"; position[] = {0,5,0};};
+        class Unit3 {side = 0; vehicle = "JLTS_Droid_B1_AR"; rank = "PRIVATE"; position[] = {-5,0,0};};
+        class Unit4 {side = 0; vehicle = "JLTS_Droid_B1_SBB3"; rank = "PRIVATE"; position[] = {-5,-5,0};};
+        class Unit5 {side = 0; vehicle = "JLTS_Droid_B1_Sniper"; rank = "PRIVATE"; position[] = {10,0,0};};
+        class Unit6 {side = 0; vehicle = "JLTS_Droid_B1_E5"; rank = "PRIVATE"; position[] = {10,5,0};};
+    };
+
+    class AUX_95th_Groups_Droid_B1_AssaultTeam_AA {
+        side = 0;
+        faction = "AUX_95th_Separatist_Army";
+        name="B1 Assault Team (AA)";
+        class Unit0 {side = 0; vehicle = "JLTS_Droid_B1_Commander"; rank = "SERGEANT"; position[] = {0,0,0};};
+        class Unit1 {side = 0; vehicle = "JLTS_Droid_B1_E5"; rank = "PRIVATE"; position[] = {5,0,0};};
+        class Unit2 {side = 0; vehicle = "JLTS_Droid_B1_Marine"; rank = "PRIVATE"; position[] = {0,5,0};};
+        class Unit3 {side = 0; vehicle = "JLTS_Droid_B1_AR"; rank = "PRIVATE"; position[] = {-5,0,0};};
+        class Unit4 {side = 0; vehicle = "JLTS_Droid_B1_SBB3"; rank = "PRIVATE"; position[] = {-5,-5,0};};
+        class Unit5 {side = 0; vehicle = "JLTS_Droid_B1_Sniper"; rank = "PRIVATE"; position[] = {10,0,0};};
+        class Unit6 {side = 0; vehicle = "JLTS_Droid_B1_E5"; rank = "PRIVATE"; position[] = {10,5,0};};
     };
 };
