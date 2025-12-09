08e919748e422058423917f3815e2c8406090b65
Turner605 <32025166+Turner605@users.noreply.github.com>
2025-12-09 10:54:14 +0000

    Adding garrison team

--- 95th-Legion-Extras/addons/units/groups/standardB1s.hpp
+++ 95th-Legion-Extras/addons/units/groups/standardB1s.hpp
@@ -70,4 +70,23 @@ class AUX_95th_Separatist_Army_B1_Standard {
         class Unit11 {side = 0; vehicle = "JLTS_Droid_B1_Sniper"; rank = "PRIVATE"; position[] = {-10,5,0};};
         class Unit12 {side = 0; vehicle = "JLTS_Droid_B1_E5"; rank = "PRIVATE"; position[] = {10,-5,0};};
     };
+
+    class AUX_95th_Groups_Droid_B1_GarrisonTeam_Large {
+        side = 0;
+        faction = "AUX_95th_Separatist_Army";
+        name="B1 Garrison Team (Large)";
+        class Unit0 {side = 0; vehicle = "JLTS_Droid_B1_Commander"; rank = "SERGEANT"; position[] = {0,0,0};};
+        class Unit1 {side = 0; vehicle = "JLTS_Droid_B1_E5"; rank = "PRIVATE"; position[] = {5,0,0};};
+        class Unit2 {side = 0; vehicle = "JLTS_Droid_B1_AT"; rank = "PRIVATE"; position[] = {0,5,0};};
+        class Unit3 {side = 0; vehicle = "JLTS_Droid_B1_AR"; rank = "PRIVATE"; position[] = {-5,0,0};};
+        class Unit4 {side = 0; vehicle = "JLTS_Droid_B1_SBB3"; rank = "PRIVATE"; position[] = {-5,-5,0};};
+        class Unit5 {side = 0; vehicle = "JLTS_Droid_B1_Sniper"; rank = "PRIVATE"; position[] = {10,0,0};};
+        class Unit6 {side = 0; vehicle = "JLTS_Droid_B1_E5"; rank = "PRIVATE"; position[] = {0,10,0};};
+        class Unit7 {side = 0; vehicle = "JLTS_Droid_B1_Marine"; rank = "PRIVATE"; position[] = {-10,0,0};};
+        class Unit8 {side = 0; vehicle = "JLTS_Droid_B1_E5"; rank = "PRIVATE"; position[] = {0,-10,0};};
+        class Unit9 {side = 0; vehicle = "JLTS_Droid_B1_AR"; rank = "PRIVATE"; position[] = {10,5,0};};
+        class Unit10 {side = 0; vehicle = "JLTS_Droid_B1_SBB3"; rank = "PRIVATE"; position[] = {5,10,0};};
+        class Unit11 {side = 0; vehicle = "JLTS_Droid_B1_Sniper"; rank = "PRIVATE"; position[] = {-10,5,0};};
+        class Unit12 {side = 0; vehicle = "JLTS_Droid_B1_E5"; rank = "PRIVATE"; position[] = {10,-5,0};};
+    };
 };
