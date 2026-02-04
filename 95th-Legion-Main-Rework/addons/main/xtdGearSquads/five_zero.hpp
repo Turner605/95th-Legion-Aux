81d0c67a92120be4a05eaf649b92a01681162f00
Valvarock <Valvarock@gmail.com>
2026-02-04 01:52:54 +0000

    flash
    folder and engineer helm added

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/five_zero.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/five_zero.hpp
@@ -1,8 +1,9 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Kong", "Hypo", "Kev"};
+    values[] = {"Kong", "Hypo", "Kev", "Flash"};
     class Kong { label = "Kong"; description = "Kong`s Helmet"; };
     class Hypo { label = "Hypo"; description = "Hypo`s"; };
     class Kev { label = "Kev"; description = "Kev`s"; };
+    class Flash { label = "Flash"; description = "Flash's"; };
 };
