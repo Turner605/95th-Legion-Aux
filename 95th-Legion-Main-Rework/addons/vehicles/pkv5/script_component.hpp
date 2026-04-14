bf6fbcc0c753e3d02da148eb345441c8c7912c37
Turner605 <32025166+Turner605@users.noreply.github.com>
2026-04-14 11:38:26 +0100

    missed files

---
+++ 95th-Legion-Main-Rework/addons/vehicles/pkv5/script_component.hpp
@@ -0,0 +1,8 @@
+#define COMPONENT vehicles_pkv5
+#define COMPONENT_BEAUTIFIED Vehicles_PKV5
+#define VEHICLE_NAME pkv5
+#define VEHICLE_NAME_BEAUTIFIED PKV5
+
+#define PARENTCOMPONENT shared
+#include "\z\AUX_95th\addons\main\script_mod.hpp"
+#include "\z\AUX_95th\addons\main\script_macros.hpp"
