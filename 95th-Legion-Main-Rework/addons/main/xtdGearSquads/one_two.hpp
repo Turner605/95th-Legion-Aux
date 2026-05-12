b5497b76fdbec4d3ede0fa063e4c643d2134f6a8
Valvarock <Valvarock@gmail.com>
2026-05-12 16:13:55 +0100

    Mates helmet
    added mates profile and helmet

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_two.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_two.hpp
@@ -1,11 +1,12 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Titan", "Boom", "Blue", "Ranger", "Pendragon", "Scootie"};
+    values[] = {"Titan", "Boom", "Blue", "Ranger", "Pendragon", "Scootie", "Mate"};
     class Titan { label = "Titan"; description = "Titan`s"; };
     class Boom { label = "Boom"; description = "Boom`s"; };
     class Blue { label = "Blue"; description = "Blue`s"; };
     class Ranger { label = "Ranger"; description = "Ranger`s Helmet"; };
     class Pendragon { label = "Pendragon"; description = "Pendragon`s"; };
     class Scootie { label = "Scootie"; description = "Scootie`s"; };
+    class Mate { label = "Mate"; description = "Mate`s"; };
 };
