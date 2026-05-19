e34b0a924cd38b7884da3e51fa7d0109ee7f1cf0
Valvarock <Valvarock@gmail.com>
2026-05-20 00:41:18 +0100

    Charlie Pilot
    added charlies pilot helmet

--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_three.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/one_three.hpp
@@ -1,11 +1,10 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"MoonShine", "Sparkie", "Jack", "Charlie", "Grunzit", "Wombat", "Steff", "Coady", "Chevi", "Canada"};
+    values[] = {"MoonShine", "Sparkie", "Jack", "Grunzit", "Wombat", "Steff", "Coady", "Chevi", "Canada"};
     class MoonShine { label = "MoonShine"; description = "MoonShine`s"; };
     class Sparkie { label = "Sparkie"; description = "Sparkie`s"; };
     class Jack { label = "Jack"; description = "Jack`s"; };
-    class Charlie { label = "Charlie"; description = "Charlie`s"; };
     class Grunzit { label = "Grunzit"; description = "Grunzit`s"; };
     class Wombat { label = "Wombat"; description = "Wombat`s"; };
     class Steff { label = "Steff"; description = "Steff`s"; };
--- 95th-Legion-Main-Rework/addons/main/xtdGearSquads/six_zero.hpp
+++ 95th-Legion-Main-Rework/addons/main/xtdGearSquads/six_zero.hpp
@@ -1,10 +1,11 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Talisman", "Token", "Kozi", "Styx"};
+    values[] = {"Talisman", "Token", "Kozi", "Styx", "Charlie"};
     class Talisman { label = "Talisman"; description = "Talisman`s"; };
     class Token { label = "Token"; description = "Token`s"; };
     class Kozi { label = "Kozi"; description = "Kozi`s"; };
     class Styx { label = "Styx"; description = "Styx`s"; };
     class Gum { label = "Gum"; description = "Gum`s"; };
+    class Charlie { label = "Charlie"; description = "Charlie`s"; };
 };
